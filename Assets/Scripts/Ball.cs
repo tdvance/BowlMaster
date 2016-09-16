bcb832ddf2aa90367f8b67569c7e7d39320fc7c1
tdvance <tdvance@gmail.com>
2016-09-16 12:39:43 -0400

    ball, camera tracking
    Added bowling ball from Udemy project for Blender
    Made ball roll with prototype rolling sound
    Made lane texture look better
    Made ball texture better
    Made pins fall when colliding
    adjusted gravity/mass/friction to make it look more realistic
    redid prefabs
    camera now follows ball down the lane

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    public float launchSpeed = 1000;
+    private AudioSource audioSource;
+    private Rigidbody rigidBody;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+        Launch();
+    }
+
+    public void Launch()
+    {
+        rigidBody.velocity = Vector3.forward * launchSpeed+Vector3.right;
+
+        audioSource.loop = false;
+        audioSource.playOnAwake = false;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+    public Ball ball;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 p = ball.transform.position + offset;
+        transform.position = new Vector3(
+            Mathf.Clamp(p.x, -100f, 100f),
+            Mathf.Clamp(p.y, 30f, 200f),
+            Mathf.Clamp(p.z, -1000f, 1829f));
+
+    }
+}
