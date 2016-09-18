fa9fcef29a561db960477b5a1bc69ea5665bd04c
tdvance <tdvance@gmail.com>
2016-09-18 17:34:11 -0400

    Animate swiper, add pin script, detect fallen pins

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+    public float fallenThreshold = 10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(name + " is standing: " + IsStanding());
+	}
+    public bool IsStanding()
+    {
+        float x = transform.eulerAngles.x;
+        float z = transform.eulerAngles.z;
+        if(x > fallenThreshold && x < 360 - fallenThreshold)
+        {
+            return false;
+        }
+        if (z > fallenThreshold && z < 360 - fallenThreshold)
+        {
+            return false;
+        }
+        return true;
+    }
+}
