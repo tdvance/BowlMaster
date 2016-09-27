d73e434b1d1dbdcbba40cf0e649a51144063453d
tdvance <tdvance@gmail.com>
2016-09-27 14:45:00 -0400

    All tests pass!

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -18,29 +18,31 @@ public class ScoreMaster {
         int[] frames = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         int[] ballsNeeded = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
         int frame = 1;
-        bool startOfFrame = true;
+        int ballsThisFrame = 0;
         foreach (int pins in rolls) {
+            ballsThisFrame++;
             for (int i = 0; i < frame; i++) {
                 if (ballsNeeded[i] > 0) {
                     ballsNeeded[i]--;
                     frames[i] += pins;
                 }
             }
-            if (startOfFrame && pins < 10) {
+            if (ballsThisFrame == 1 && pins < 10) {
                 ballsNeeded[frame - 1]++;
-                startOfFrame = false;
             } else {
-                if (startOfFrame && pins == 10) { //strike
+                if (ballsThisFrame == 1 && pins == 10) { //strike
                     ballsNeeded[frame - 1] += 2;
-                }else if (frames[frame - 1] == 10) {//spare
+                } else if (frames[frame - 1] == 10 && ballsThisFrame == 2 && ballsNeeded[frame - 1] == 0) {//spare
                     ballsNeeded[frame - 1]++;
                 }
-                startOfFrame = true;
-                frame++;
+                if (frame < 10) {
+                    ballsThisFrame = 0;
+                    frame++;
+                }
             }
         }
         List<int> frameList = new List<int>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {//only return complete frames
             if (ballsNeeded[i] == 0) {
                 frameList.Add(frames[i]);
             }
